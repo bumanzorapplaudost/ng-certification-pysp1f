52cec5ab842b4178f55cfa099f8c503b7a4695fb
Gerson Umanzor <gbumanzor@outlook.com>
2021-09-04 22:04:35 -0600

    Separate components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
+import {WeatherComponent} from "./components/weather/weather.component";
+import {ForecastComponent} from "./components/forecast/forecast.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: WeatherComponent
+    },
+    {
+        path: 'forecast/:zipCode',
+        component: ForecastComponent
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
